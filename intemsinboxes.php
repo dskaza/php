c81e2ef3eb3e97db0ffc1de53c714b4b18cc6231
Denis <dskaza@gmail.com>
2015-08-17 22:53:32 +0200

    Update intemsinboxes.php

--- intemsinboxes.php
+++ intemsinboxes.php
@@ -1,9 +1,106 @@
 <?php
 
+class ItemsInBoxes
+{
+
+  public function getAppropriateBoxes($items, $boxes)
+  {
+    $choosenBoxes = array();
+
+	foreach( $items as $itemId => $neededQuantity )	
+    {
+      foreach( $boxes as $boxId => $products )
+      {
+        if( isset($boxes[$boxId][$itemId]) )
+        {
+          $choosenBoxes[] = $boxId;
+        }
+      }
+    }
+
+    return array_values(array_unique($choosenBoxes));
+  }
+
+
+  public function run(&$items, &$neededBoxes, &$boxes)
+  {
+    $boxIndex = array_rand($neededBoxes, 1);
+    $boxId = $neededBoxes[$boxIndex];
+
+    $box = $boxes[$boxId];
+    $requiredBoxes = 0;
+    
+    foreach( $box as $productId => $availableQuantity )
+    {
+      // if box has item that we don't need skip it
+      if( !isset($items[$productId]) )
+        continue;
+
+	     $neededQuantity = $items[$productId];
+
+      // item quantity has already been satisfied, but the box has this item
+      // we can skip this product then
+      if( $neededQuantity < 1 )
+      {
+        continue;
+      }
+      
+      $requiredBoxes_ = ceil( $neededQuantity / $availableQuantity );
+
+      if( $requiredBoxes_ > $requiredBoxes )
+        $requiredBoxes = $requiredBoxes_;
+
+      // decrease item to 0
+      $items[$productId] = 0;
+    }
+
+    // we took everything that we need from this box, now 
+    // we can delete it
+    unset($neededBoxes[$boxIndex]);
+
+    // re-index
+    $neededBoxes = array_values($neededBoxes);
+
+    return array($boxId => $requiredBoxes);
+  }
+
+
+  public function allDone($items) 
+  {
+    foreach( $items as $itemId => $neededQuantity ){
+      if( $neededQuantity > 0 )    
+        return false;      
+    }
+    return true;
+  }
+
+	
+  public function getFinalResult($result)
+  {
+    $finalResult = array();
+    foreach( $result as $index => $box )    
+      foreach( $box as $boxId => $neededBoxes )     
+        if( $neededBoxes > 0 )        
+          $finalResult[$boxId] = $neededBoxes;                  
+
+    return $finalResult;    
+  }
+
+}
+
+
+// ---------------------------------------------------------------------------
+
+
+
+//include("algorithm.php");
+
 /*
 items array is [$itemId => $neededQuantity]
 This array has the list of items and quantities which I need to buy.
 */
+//$items = require_once('items.php');
+
 $items = array (
   '3649718740' => '7',
   '4063792958' => '10',
@@ -40,29 +137,13 @@ $items = array (
   '6028757095' => '10',
   '6028793024' => '12'
 );
-
-
-echo count($items);
-
-echo '<br>';
-
-print("<pre>".print_r($items,true)."</pre>");
-
-var_dump($items);
-
-/*
-print "<pre>";
-print_r($items);
-print "</pre>";
-*/	
-echo '<br>';
-
-
+// 34 items
 
 /*
 boxes array is $boxId => [$itemId => $quantityInBox]
 This array has the catalog of boxes available to buy.
 */
+//$boxes = require_once('boxes.php');
 $boxes = array (
   '4715223106' =>
       array (
@@ -183,8 +264,12 @@ $boxes = array (
         '6028793024' => '1',
       ),
 );
+// 25 boxes
 
-//echo $boxes;
+
+//-----------------------------------------------------------------
+
+
 /*
 We have boxes which have N items in it with N quantity. Items can be in 1 or more boxes.
 How many boxes from which box do I need to buy, to satisfy the needed item quantities?
@@ -193,14 +278,46 @@ What would be the best combination to have least amount of boxes.
 An OOP approach would add more point to the test.
 */
 
-/*
-foreach($boxes[0] as $child) {
-	echo $child . "\n";	
-}
-*/
+$obj = new ItemsInBoxes();
 
-foreach ($boxes as $i => $value) {
-	foreach ($items as $n => $value) {
-		echo $items[$n] . "<br>";	
-	}
+// traverse throught all items and get array of boxes in which they are
+//
+$neededBoxes = $obj->getAppropriateBoxes($items, $boxes);
+
+
+$result = array();
+$count = count($neededBoxes);
+	
+while( $count > 0 )
+{
+  $result[] = $obj->run($items, $neededBoxes, $boxes);
+
+  // if all items are satisfied we have solution break
+  if( $obj->allDone($items) )
+  {
+    // We found solution 
+    break;
+  }
+  $count = count($neededBoxes);
+}
+
+$finalResult = $obj->getFinalResult($result);
+
+$boxCount = 0;
+foreach( $finalResult as $boxId => $boxNumber)
+  $boxCount += $boxNumber;
+
+
+echo "Boxes with number to buy and items in it (listed in format BoxId => Amount[list of Items]) : \n\n" . '<br>';
+foreach( $finalResult as $boxId => $boxNumber )
+{
+  echo $boxId . " => " . $boxNumber . "   [";
+  $index = 1;
+  foreach ($boxes[$boxId] as $itemId => $quantity)  
+  {
+	  if ($index > 1) echo ", ";
+	  echo $itemId;  
+	  $index ++;
+  }
+  echo "]" . '<br>';
 }
